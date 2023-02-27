676d698efe1a46413ba84f0c85c164192a5ad1fe
Ayshwarya Jagadeesan <ayshjag@msla0669.attlocal.net>
2023-02-27 15:38:36 -0500

    Modified think pudding to accomodate new social comparators and cedar genrated causalpathways

--- thinkpudding.py
+++ thinkpudding.py
@@ -35,17 +35,23 @@ class Thinkpudding:
     
     def process_causalpathways(self):
         start_time = time.time()
+        
         self.caus_out_dict=process_causalpathways(self.causal_pathways)
+        # for k,v in self.caus_out_dict.items():
+        #     print(k,v)
         logging.critical(" processing causal pathways--- %s seconds ---" % (time.time() - start_time))
 
     def process_spek(self):
         start_time = time.time()
-        self.spek_out_dicts=process_spek(self.spek_cs)
+        self.spek_out_dicts,self.comparator_dicts=process_spek(self.spek_cs)
         logging.critical(" processing spek_cs--- %s seconds ---" % (time.time() - start_time))
     
     def matching(self):
         start_time = time.time()
-        self.merged_list=matching(self.caus_out_dict,self.spek_out_dicts)
+        matching(self.caus_out_dict,self.spek_out_dicts,self.comparator_dicts)
+        # self.merged_list=matching(self.caus_out_dict,self.spek_out_dicts)
+        # for x in range(len(self.merged_list)):
+        #     print(self.merged_list[x])
         logging.critical(" processing matching--- %s seconds ---" % (time.time() - start_time))
 
     def insert(self):
